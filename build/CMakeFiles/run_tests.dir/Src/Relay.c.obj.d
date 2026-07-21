a929947445a04229cfdecdf22b493b8bba272fa2
pawel <madon.pawel@gmail.com>
2026-07-21 21:32:46 +0200

    removal of cached build folder

--- build/CMakeFiles/run_tests.dir/Src/Relay.c.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/run_tests.dir/Src/Relay.c.obj: \
- X:\Projects\FOC_Driver\Firmware\Src\Relay.c \
- X:/Projects/FOC_Driver/Firmware/Inc/Relay.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
- c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- X:/Projects/FOC_Driver/Firmware/Inc/ADC_Service.h
