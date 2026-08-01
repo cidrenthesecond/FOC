0c944cca14dee1f9a781bc054c3385609d5523f8
pawel <madon.pawel@gmail.com>
2026-08-01 21:23:21 +0200

    CMSIS separation UFFFF

--- Tests/CMSIS/AllTests.c
+++ Tests/CMSIS/AllTests.c
@@ -4,10 +4,6 @@ static void RunAllTests(void)
 {   
     printf("\nCMSIS tests running...\n");
     RUN_TEST_GROUP(CMSIS);
-    printf("\nFakeMicroTime tests running...\n");
-    RUN_TEST_GROUP(FakeMicroTime);
-    printf("\nSVPWM tests running...\n");
-    RUN_TEST_GROUP(SVPWM);
 }
 
 int main(int argc, const char * argv[])
