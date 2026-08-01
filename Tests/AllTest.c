bb86ed7efff25e068b3ba02bbbdf122af5f982e3
pawel <madon.pawel@gmail.com>
2026-08-01 21:15:08 +0200

    Logger separation

--- Tests/AllTest.c
+++ Tests/AllTest.c
@@ -4,20 +4,10 @@ static void RunAllTests(void)
 {   
     printf("\nCMSIS tests running...\n");
     RUN_TEST_GROUP(CMSIS);
-    printf("\nFake_DcLink tests running...\n");
-    RUN_TEST_GROUP(Fake_DcLink);
-    printf("\nRelay tests running...\n");
-    RUN_TEST_GROUP(Relay);
-    printf("\nPID tests running...\n");
-    RUN_TEST_GROUP(PID);
     printf("\nFakeMicroTime tests running...\n");
     RUN_TEST_GROUP(FakeMicroTime);
     printf("\nSVPWM tests running...\n");
     RUN_TEST_GROUP(SVPWM);
-    printf("\nFIFO tests running...\n");
-    RUN_TEST_GROUP(FIFO_Test);
-    printf("\nLogger tests running...\n");
-    RUN_TEST_GROUP(Logger_Test);
 }
 
 int main(int argc, const char * argv[])
