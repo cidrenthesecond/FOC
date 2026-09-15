cba5d09f67156a3a04b8a718778c0f4ab5b4897d
pawel <madon.pawel@gmail.com>
2026-09-15 22:08:04 +0200

    Finish of sys tick dispatcher tests

--- Tests_Automatic/SysTickDispatcher/SysTickDispatcher_TestRunner.c
+++ Tests_Automatic/SysTickDispatcher/SysTickDispatcher_TestRunner.c
@@ -2,15 +2,16 @@
 
 TEST_GROUP_RUNNER(SysTickDispatcher)
 {
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenNotInitialized_ReturnsNoInitError);
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenNoTasksAreSubscribed_ReturnsNoTasksError);
     RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenPeriodIsGreaterThanZero_ReturnsSuccess);
     RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenPeriodIsZero_ReturnsFail);
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenPeriodHasNotElapsed_DoesNotDispatchTask);
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenPeriodHasElapsed_DispatchesTask);
     RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenTimerIsAlreadyRunning_StartsTaskPeriodFromSubscription);
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenTaskIsSubscribed_DispatchesTaskEveryPeriod);
-    RUN_TEST_CASE(SysTickDispatcher, Init_WhenCalled_ClearsAllSubscriptions);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenNoInit_ReturnsFail);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenTaskIsNull_ReturnsFail);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_CorrectArguments_ReturnsSuccess);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_CanSubscribeMaxTasks);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenOverMaxTasks_ThrowsError);
+    RUN_TEST_CASE(SysTickDispatcher, Subscribe_WhenRegisteringSameTask_ThrowsError);
+
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenNotInitialized_ReturnsNoInitError);
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenNoTasksAreSubscribed_ReturnsNoTasksError);
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenTaskIsSubscribed_ReturnsSuccess);
@@ -18,7 +19,19 @@ TEST_GROUP_RUNNER(SysTickDispatcher)
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenTaskIsNotSubscribed_ReturnsFail);
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenPeriodDoesNotMatch_ReturnsFail);
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenOperationFails_KeepsTaskSubscribed);
-    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenMultipleTasksAreSubscribed_DispatchesEachAccordingToItsPeriod);
     RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenTaskHasElapsedPartOfPeriod_DoesNotDispatchTask);
+    RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenTaskIsNull_ReturnFail);
+    RUN_TEST_CASE(SysTickDispatcher, Unsubscribe_WhenPeriodIsZeroAndTaskIsNull_ReturnFail);
+    RUN_TEST_CASE(SysTickDispatcher, CanUnsubscribeMAX);
+
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenNotInitialized_ReturnsNoInitError);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenNoTasksAreSubscribed_ReturnsNoTasksError);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenPeriodHasNotElapsed_DoesNotDispatchTask);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenPeriodHasElapsed_DispatchesTask);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenTaskIsSubscribed_DispatchesTaskEveryPeriod);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenMultipleTasksAreSubscribed_DispatchesEachAccordingToItsPeriod);
     RUN_TEST_CASE(SysTickDispatcher, Wakeup_WhenLastTaskIsUnsubscribed_ReturnsNoTasksError);
+    RUN_TEST_CASE(SysTickDispatcher, Wakeup_AllTasksCanBeCalled);
+
+    RUN_TEST_CASE(SysTickDispatcher, Init_WhenCalled_ClearsAllSubscriptions);
 }
