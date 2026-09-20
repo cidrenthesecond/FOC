cd4281482634e7e108021dd4ece31d066e498a27
pawel <madon.pawel@gmail.com>
2026-09-20 18:20:48 +0200

    Filter onFirstData and reset added

--- Tests_Manual/NucleoBoard/TODO/Moving_Avarage_Filter.c
+++ Tests_Manual/NucleoBoard/TODO/Moving_Avarage_Filter.c
@@ -1,4 +1,5 @@
 #include "Moving_Avarage_Filter.h"
+#include <cstdint>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,7 +10,7 @@ typedef struct MovingAvarage_struct{
     uint32_t sum;
     uint8_t kernel;
     uint8_t putIndex;
-    uint8_t getIndex;
+    uint8_t gotFirstSample;
 }MovingAvarage_struct;
 
 MovingAvarage MovingAvarage_Init(const uint8_t kernel)
@@ -23,14 +24,27 @@ MovingAvarage MovingAvarage_Init(const uint8_t kernel)
     result -> sum = 0;
     result -> kernel = kernel;
     result -> putIndex = 0;
-    result -> getIndex = 0;
+    result -> gotFirstSample = 0;
     return result;
+}
+
+static uint16_t OnFirstSample(MovingAvarage f, uint16_t sample)
+{
+    for(uint8_t i = 0; i< f->kernel; i++)
+        f->dataPtr[i] = sample;
+
+    f->gotFirstSample = 1;
+    f->sum = sample*(f->kernel);
+    return sample;
 }
 
 uint16_t      MovingAvarage_Filter(MovingAvarage filter, uint16_t inputSample)
 {
     if( filter == NULL || filter -> dataPtr == NULL ) 
         return ERROR_CODE;
+
+    if(!(filter->gotFirstSample))
+        return OnFirstSample(filter,inputSample);
 
     filter -> sum -= filter -> dataPtr[filter -> putIndex];
     filter -> dataPtr[filter->putIndex] = inputSample;
@@ -39,6 +53,14 @@ uint16_t      MovingAvarage_Filter(MovingAvarage filter, uint16_t inputSample)
     filter -> putIndex = (filter -> putIndex + 1) % filter -> kernel;
 
     return (uint16_t)(filter->sum / filter->kernel);
+}
+
+void MovingAvarage_Reset(MovingAvarage filter)
+{
+    memset((filter->dataPtr), 0, sizeof(uint16_t)*(filter -> kernel));
+    filter -> sum = 0;
+    filter -> putIndex = 0;
+    filter -> gotFirstSample = 0;
 }
 
 void          MovingAvarage_Destroy(MovingAvarage filter)
--- Tests_Manual/NucleoBoard/TODO/Moving_Avarage_Filter.h
+++ Tests_Manual/NucleoBoard/TODO/Moving_Avarage_Filter.h
@@ -7,6 +7,7 @@ typedef struct MovingAvarage_struct * MovingAvarage;
 
 MovingAvarage MovingAvarage_Init(const uint8_t kernel);
 uint16_t      MovingAvarage_Filter(MovingAvarage filter, uint16_t inputSample);
+void          MovingAvarage_Reset(MovingAvarage filter);
 void          MovingAvarage_Destroy(MovingAvarage filter);
 
 #endif
